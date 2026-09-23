d6e5e01bf03b8cb33b83991c6cb4d9954570c6b6
Mike Foster <i.am.mike.foster@gmail.com>
2026-09-23 10:41:07 +0000

    empty cancel

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,15 @@
 //! use thread_safely::prelude::*;
 //! // Set up a [Controller] and (clonable) [Context]
 //! let (workerthreads, keepalive): (Controller, Context) = Controller::new();
+//!
+//! // set up a load of threads
+//! // ...
+//!
+//! // cancel the workers when something happens
+//! workerthreads.cancel();
+//!
+//! // You still need to join your threads before you finish
+//!
 //! ```
 
 pub mod prelude {
@@ -23,4 +32,6 @@ impl Controller {
     pub fn new() -> (Controller, Context) {
         (Controller {}, Context {})
     }
+
+    pub fn cancel() {}
 }
